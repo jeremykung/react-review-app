0a7934d45742f5ba7866688573677e7b7a83864e
se <user@Userde-MacBook-Pro.local>
2022-02-14 19:51:18 +0800

    rebuild

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header"
 import FeedbackForm from "./components/FeedbackForm"
 import FeedbackList from "./components/FeedbackList"
-import feedbackData from "./data/FeedbackData"
+// import feedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 // import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
